ecc5c04819c8d6654efe01437f52b6b856cd2f5d
spiril1000 <martin.jokumsen@gmail.com>
2020-11-18 16:02:37 +0100

    Tilbage på master

---
+++ src/store/Index.js
@@ -0,0 +1,24 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+export default new Vuex.Store({
+    state: {
+        // Stores the toke and user object
+        user: null,
+        token: null,
+        // baseadress: "https://localhost:44382/api/",
+        baseadress: "http://hairforceone.azurewebsites.net/api/"
+    },
+    mutations: {
+        setUser(state, user) {
+            state.user = user;
+        },
+        setToken(state, token) {
+            state.token = token;
+        },
+    },
+    actions: {},
+    getters: {},
+},
+);
