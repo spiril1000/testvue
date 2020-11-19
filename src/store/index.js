bef8845b4cfc4f8a5b4693ecf75b2b86b3d6e70a
spiril1000 <martin.jokumsen@gmail.com>
2020-11-20 00:28:35 +0100

    sdfjksdjkf

--- src/store/index.js
+++ src/store/index.js
@@ -1,24 +1,42 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from "vue";
+import Vuex from "vuex";
+import axios from "axios";
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 export default new Vuex.Store({
-    state: {
-        // Stores the toke and user object
-        user: null,
-        token: null,
-        // baseadress: "https://localhost:44382/api/",
-        baseadress: "http://hairforceone.azurewebsites.net/api/"
+  state: {
+    // Stores the toke and user object
+    user: null,
+    users: null,
+    token: null,
+    // baseadress: "https://localhost:44382/api/",
+    baseadress: "http://hairforceone.azurewebsites.net/api/",
+  },
+  mutations: {
+    setUser(state, user) {
+      state.user = user;
     },
-    mutations: {
-        setUser(state, user) {
-            state.user = user;
-        },
-        setToken(state, token) {
-            state.token = token;
-        },
+    setUsers(state, users) {
+      state.users = users;
     },
-    actions: {},
-    getters: {},
-},
-);
+    setToken(state, token) {
+      state.token = token;
+    },
+  },
+  actions: {
+    async getUsers({ commit }) {
+      try {
+        const response = await axios.get("/users");
+        commit("setUsers", response.data);
+      } catch (error) {
+        console.log(error);
+        this.$router.push("/login");
+      }
+    },
+  },
+  getters: {
+    users: (state) => {
+      return state.users;
+    },
+  },
+});
