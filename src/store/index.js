3df63c47f77941b24af21ed8c14d7f14a70e4cf1
spiril1000 <martin.jokumsen@gmail.com>
2020-11-19 22:51:21 +0100

    Axios

--- src/store/index.js
+++ src/store/index.js
@@ -1,24 +1,29 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from "vue";
+import Vuex from "vuex";
+import axios from "axios";
 
-Vue.use(Vuex)
+axios.defaults.baseURL = "https://hairforceone.azurewebsites.net/api/";
+axios.defaults.headers.common["Authorization"] =
+  "Bearer " + sessionStorage.getItem("Token");
+axios.defaults.headers.post["Content-Type"] = "application/json";
+
+Vue.use(Vuex);
 export default new Vuex.Store({
-    state: {
-        // Stores the toke and user object
-        user: null,
-        token: null,
-        // baseadress: "https://localhost:44382/api/",
-        baseadress: "http://hairforceone.azurewebsites.net/api/"
+  state: {
+    // Stores the toke and user object
+    user: null,
+    token: null,
+    // baseadress: "https://localhost:44382/api/",
+    baseadress: "http://hairforceone.azurewebsites.net/api/",
+  },
+  mutations: {
+    setUser(state, user) {
+      state.user = user;
     },
-    mutations: {
-        setUser(state, user) {
-            state.user = user;
-        },
-        setToken(state, token) {
-            state.token = token;
-        },
+    setToken(state, token) {
+      state.token = token;
     },
-    actions: {},
-    getters: {},
-},
-);
+  },
+  actions: {},
+  getters: {},
+});
