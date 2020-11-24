e34f9558eafa60fc32b3d5a651cf08961a1196c3
spiril1000 <martin.jokumsen@gmail.com>
2020-11-24 13:10:11 +0100

    axios localhost virker

--- src/axios/index.js
+++ src/axios/index.js
@@ -1,11 +1,13 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseUrl: "https://localhost:44382/api/",
-  // baseURL: "https://hairforceone.azurewebsites.net/api/",
+  // baseURL: "https://localhost:44382/api/",
+  baseURL: "https://hairforceone.azurewebsites.net/api/",
 });
 
 instance.defaults.headers.common["Authorization"] =
   "Bearer " + sessionStorage.getItem("Token");
+
+instance.defaults.headers.post["Content-Type"] = "application/Json";
 
 export default instance;
